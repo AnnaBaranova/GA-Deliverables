7e1b1c05a65e4a347f5274f1297e8950f06cbf31
Anna Baranova <asbaranova1@gmail.com>
2021-03-11 16:11:01 -0500

    add task 4

--- DOM_Lab/script.js
+++ DOM_Lab/script.js
@@ -27,4 +27,33 @@ var menuLinks = [
     aEl.textContent = el.text;
     topMenuEl.appendChild(aEl);
   })
+
+  
+  const subMenuEl = document.getElementById("sub-menu");
+  subMenuEl.style.height = "100%";
+  subMenuEl.style.backgroundColor = "var(--sub-menu-bg)";
+  subMenuEl.classList.add("flex-around");
+  subMenuEl.style.position = "absolute";
+  subMenuEl.style.top = "0";
+
+  const menuLinks = [
+    {text: 'about', href: '/about'},
+    {text: 'catalog', href: '#', subLinks: [
+      {text: 'all', href: '/catalog/all'},
+      {text: 'top selling', href: '/catalog/top'},
+      {text: 'search', href: '/catalog/search'},
+    ]},
+    {text: 'orders', href: '#' , subLinks: [
+      {text: 'new', href: '/orders/new'},
+      {text: 'pending', href: '/orders/pending'},
+      {text: 'history', href: '/orders/history'},
+    ]},
+    {text: 'account', href: '#', subLinks: [
+      {text: 'profile', href: '/account/profile'},
+      {text: 'sign out', href: '/account/signout'},
+    ]},
+  ];
+
+
+
   
