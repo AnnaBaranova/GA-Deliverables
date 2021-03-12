7a0b1f0e0ee7193c7b2f1e421823e1e616e69ea2
Anna Baranova <asbaranova1@gmail.com>
2021-03-12 17:12:50 -0500

    finished DOM_Lab

--- DOM_Lab/script.js
+++ DOM_Lab/script.js
@@ -62,25 +62,68 @@ let showingSubMenu = false;
 topMenuEl.addEventListener("click", function (e) {
   e.preventDefault();
   let linkName = e.target;
+  let clickText = linkName.textContent;
   if (linkName.tagName === "A") {
-    console.log(linkName.textContent)
+    console.log(clickText);
   } else {
     console.log("wrong click");
+    mainEl.innerHTML = "<h1>SEI Rocks!</h1>";
+    return
   };
 
   if (linkName.classList.contains("active")) {
     linkName.classList.remove("active");
     showingSubMenu = false;
     subMenuEl.style.top = "0";
-    return 
+    return
   }
-  topMenuLinks.forEach (function (el) {
+
+  topMenuLinks.forEach(function (el) {
     el.classList.remove("active");
   });
   linkName.classList.add("active");
-  if (menuLinks["subLinks"]){
+  let subLinks
+  menuLinks.forEach(function (el) {
+    if (el.text === clickText && el.subLinks) {
+      subLinks = el.subLinks;
+    }
+  });
+  if (subLinks) {
     showingSubMenu = true;
+    buildSubMenu(subLinks);
+    subMenuEl.style.top = "100%";
   } else {
     showingSubMenu = false;
+    subMenuEl.style.top = "0";
+    mainEl.innerHTML = `<h1>${clickText}</h1>`;
   }
 });
+function buildSubMenu(arr) {
+  subMenuEl.innerHTML = "";
+  arr.forEach(function (el) {
+    const aEl = document.createElement("a");
+    aEl.setAttribute("href", el.href);
+    aEl.textContent = el.text;
+    subMenuEl.appendChild(aEl);
+  });
+}
+
+subMenuEl.addEventListener("click", function (e) {
+  e.preventDefault();
+  let linkName = e.target;
+  let clickText = linkName.textContent;
+  if (linkName.tagName === "A") {
+    console.log(clickText);
+    showingSubMenu = false;
+    subMenuEl.style.top = "0";
+    topMenuLinks.forEach(function (el) {
+      el.classList.remove("active");
+    });
+    mainEl.innerHTML = `<h1>${clickText}</h1>`;
+
+  } else {
+    console.log("wrong click again");
+    mainEl.innerHTML = "<h1>SEI Rocks!</h1>";
+  }
+});
+
