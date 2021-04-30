95781336b19898c38676443f0962f48f03ab1284
Anna Baranova <asbaranova1@gmail.com>
2021-04-30 16:09:07 -0400

    add create new flight function and partials

--- mongoose-flights/controllers/flights.js
+++ mongoose-flights/controllers/flights.js
@@ -6,16 +6,27 @@ function allFlights(req, res){
         if (err) {
             return console.log(err);
         }
-        res.render('flights/index', {flights});
+        res.render('flights/index', { title: 'All Flights', flights});
     })
 };
 
 function newFlight(req, res) {
-    res.render('flights/new');
+    res.render('flights/new', { title: 'Add Flight' });
 };
+
+function create (req, res){
+    const flight = new Flight(req.body);
+    console.log(flight);
+    flight.save(function (err) {
+        if (err) return res.redirect('/flights/new');
+        res.redirect('/flights');
+      });
+
+}
 
 
 module.exports = {
     index: allFlights,
     new: newFlight,
+    create
 }
--- mongoose-flights/routes/flights.js
+++ mongoose-flights/routes/flights.js
@@ -5,6 +5,7 @@ const flightsCtrl = require('../controllers/flights');
 router.get('/', flightsCtrl.index);
 
 router.get('/new', flightsCtrl.new);
+router.post('/', flightsCtrl.create);
 
 
 module.exports = router;
