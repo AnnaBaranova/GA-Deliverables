a56d7bc07cf5cf6b947a08909696b050566c84ba
Anna Baranova <asbaranova1@gmail.com>
2021-05-02 00:31:33 -0400

    fix ticket form

--- mongoose-flights/controllers/tickets.js
+++ mongoose-flights/controllers/tickets.js
@@ -2,10 +2,19 @@ const Flight = require('../models/flight');
 const Ticket = require('../models/ticket');
 
 function newTicket(req, res) {
-    res.render('tickets/new', { title: 'Add Ticket' });
+    console.log(req.params);
+    res.render('tickets/new', { title: 'Add Ticket', flightId: req.params.id });
 }
 
+function create (req, res){
+    console.log(req.params.id);
+    const flightId = req.params.id;
+    const ticket = new Ticket(req.body);
+    console.log(ticket);
+    res.redirect(`/flights/${flightId}`);
+};
 
 module.exports = {
     new: newTicket,
+    create,
 }
--- mongoose-flights/routes/tickets.js
+++ mongoose-flights/routes/tickets.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const ticketsCtrl = require('../controllers/tickets');
 
-router.get('/tickets/new', ticketsCtrl.new);
+router.get('/flights/:id/tickets/new', ticketsCtrl.new);
+router.post('/flights/:id/tickets', ticketsCtrl.create)
 
 module.exports = router;
