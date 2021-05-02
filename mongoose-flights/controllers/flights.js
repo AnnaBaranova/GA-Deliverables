8c08fa5eefcbc4d2c39b1ea5d0462fa62c7ab794
Anna Baranova <asbaranova1@gmail.com>
2021-05-02 09:57:00 -0400

    add create ticket function

--- mongoose-flights/controllers/flights.js
+++ mongoose-flights/controllers/flights.js
@@ -2,13 +2,13 @@ const Flight = require('../models/flight');
 const Ticket = require('../models/ticket');
 
 
-function allFlights(req, res){
+function allFlights(req, res) {
     Flight.find({}, function (err, flights) {
 
         if (err) {
             return console.log(err);
         }
-        res.render('flights/index', { title: 'All Flights', flights});
+        res.render('flights/index', { title: 'All Flights', flights });
     })
 };
 
@@ -16,21 +16,21 @@ function newFlight(req, res) {
     res.render('flights/new', { title: 'Add Flight' });
 };
 
-function create (req, res){
+function create(req, res) {
     const flight = new Flight(req.body);
     console.log(flight);
     flight.save(function (err) {
         if (err) return res.redirect('/flights/new');
         res.redirect('/flights');
-      });
+    });
 
 };
 
-function show (req, res) {
+function show(req, res) {
     Flight.findById(req.params.id, function (err, flight) {
-        Ticket.find({flight: flight._id}, function(err, tickets) {
-        res.render('flights/show', { title: 'Flight Detail', flight, tickets });
-});
+        Ticket.find({ flight: flight._id }, function (err, tickets) {
+            res.render('flights/show', { title: 'Flight Detail', flight, tickets });
+        });
     });
 };
 
--- mongoose-flights/controllers/tickets.js
+++ mongoose-flights/controllers/tickets.js
@@ -2,16 +2,17 @@ const Flight = require('../models/flight');
 const Ticket = require('../models/ticket');
 
 function newTicket(req, res) {
-    console.log(req.params);
     res.render('tickets/new', { title: 'Add Ticket', flightId: req.params.id });
 }
 
 function create (req, res){
     console.log(req.params.id);
     const flightId = req.params.id;
-    const ticket = new Ticket(req.body);
-    console.log(ticket);
-    res.redirect(`/flights/${flightId}`);
+   const ticket = new Ticket(req.body);
+    ticket.flight = flightId;
+    ticket.save(function(err){
+      res.redirect(`/flights/${flightId}`);  
+    })  
 };
 
 module.exports = {
